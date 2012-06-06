5d72b04cccbaa8e67c82d52942aa7c1df696b70a
Joe LaPayne <jrlapayne@gmail.com>
2012-06-06 14:29:58 -0500

    2:29pmJune6

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Arc2::Application.config.secret_token = 'e2f74026c896d50b6fbd06746408a5923ef8f92d76c1f61ec788e0b6bc849d7ae793e32537737da5a22144944438bb2b0a6926f64d676c83789f4f7664f792fd'
